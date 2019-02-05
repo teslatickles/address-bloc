e045bc5bbae83c403594a973eb81464efa4af13a
teslatickles <hunterhartline@gmail.com>
2019-02-04 22:41:11 -0600

    add jasmine and create first test suite

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -61,4 +61,8 @@ module.exports = class MenuController {
         console.log("Thanks for using AddressBloc!");
         process.exit();
     }
+
+    getContactCount() {
+        return this.contacts.length;
+    }
 }
